f79847f6c1bf02b920b01c7fab0af62fc1611785
Cryspysam <ssmishra9918@gmail.com>
2025-07-08 23:51:30 +0530

    Updated Lottery component and added CSS styling

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,11 @@ import { useState } from 'react';
 import './App.css';
 import TodoList from './TodoList';
 import Lottery from './Lottery';
-import Ticket from './Ticket';
 
 function App() {
   return (
     <>
-      <Ticket ticket={[0, 1, 2]} />
-      <Ticket ticket={[0, 1, ,5,2]} />
+     <Lottery n={3} winningSum={15}/>
     </>
   );
 }
--- src/Lottery.jsx
+++ src/Lottery.jsx
@@ -1,23 +1,19 @@
 import { useState } from "react"
-import "./Lottery.css";
 import { genTicket, sum } from "./helper";
+import Ticket from "./Ticket";
 
-export default function Lottery() {
-    let [ticket, setTicket]= useState(genTicket(3));
-    let isWinning = sum(ticket) === 15
+export default function Lottery({n=3, winningSum=15}) {
+    let [ticket, setTicket]= useState(genTicket(n));
+    let isWinning = sum(ticket) === winningSum;
 
       let buyTicket = () =>{
-        setTicket(genTicket(3));
+        setTicket(genTicket(n));
       }
 
     return(
         <div>
             <h1>Lottery Game!</h1>
-            <div className="ticket">
-                <span>{ticket[0]}</span> 
-                <span>{ticket[1]}</span>
-                <span>{ticket[2]}</span>
-            </div>
+            <Ticket ticket={ticket}/>
             <br />
             <button onClick={buyTicket}>buy New ticket</button>
             <h3>{isWinning && "Congrectulations, you won!"}</h3>
