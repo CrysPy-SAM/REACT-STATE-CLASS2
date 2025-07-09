cc33804330deb2f3f899d279be4b6cfb2c5677d6
Cryspysam <ssmishra9918@gmail.com>
2025-07-09 21:58:35 +0530

    assign Forms

--- Form.jsx
+++ Form.jsx
@@ -1,16 +1,66 @@
-import { useState } from "react"
+import { useState } from "react";
 
 export default function Form() {
-    let {fullName, setFullName} = useState("Satyam");
+    let [formData, setFormData] = useState({
+        fullName: "",
+        username: "",
+        password: ""
+    });
 
-    let handleNameChange = (event) =>{
-        setFullName(event.target.value);
-    }
+    let handleInputChange = (event) => {
+        const { name, value } = event.target;
+        setFormData((currData) => ({
+            ...currData,
+            [name]: value
+        }));
+    };
 
-    return(
-        <form>
-            <input placeholder="enter your full name" type="text" value={fullName} onChange={handleNameChange}/>
-            <button>Submit</button>
+    let handleSubmit = (event) => {
+        event.preventDefault();
+        console.log(formData);
+        setFormData({
+            fullName: "",
+            username: "",
+            password: ""
+        });
+    };
+
+    return (
+        <form onSubmit={handleSubmit}>
+            <label htmlFor="fullName">Full Name</label>
+            <input
+                placeholder="Enter full name"
+                type="text"
+                value={formData.fullName}
+                id="fullName"
+                name="fullName"
+                onChange={handleInputChange}
+            />
+            <br /><br />
+
+            <label htmlFor="username">Username</label>
+            <input
+                placeholder="Enter username"
+                type="text"
+                value={formData.username}
+                id="username"
+                name="username"
+                onChange={handleInputChange}
+            />
+            <br /><br />
+
+            <label htmlFor="password">Password</label>
+            <input
+                placeholder="Enter your password"
+                type="password"
+                value={formData.password}
+                id="password"
+                name="password"
+                onChange={handleInputChange}
+            />
+            <br /><br />
+
+            <button type="submit">Submit</button>
         </form>
-    )
+    );
 }
